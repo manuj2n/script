2ce0100ab5b369d21ddcdfb823eaa652b0a381b9
manuj2n <manu.j2n@gmail.com>
2018-06-07 11:36:15 +0200

    add

--- serveur-vlc-http.sh
+++ serveur-vlc-http.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-raspivid -w 640 -h 380 -fps 10 -b 120000 -n -t 0 -o -| cvlc -vvv stream://dev/stdin --sout '#standart{access=http,mux=ts,dst=:8090}' :demux=h264 &
+raspivid -w 640 -h 380 -fps 10 -b 120000 -f -t 0 -o -| cvlc -vvv stream:///dev/stdin --sout '#standart{access=http,mux=ts,dst=:8090}' :demux=h264 &
 
